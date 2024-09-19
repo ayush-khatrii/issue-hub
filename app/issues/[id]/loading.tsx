a22ddfba3b67ebdf273bc162a96e16a1320e7729
Ayush Khatri <khatriayush2003@gmail.com>
2024-09-20 03:32:13 +0530

    Added skeleton loading for issue details page

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,19 @@
+import { Card } from '@nextui-org/react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+const SingleIssueLoading = () => {
+  return (
+    <section className='max-w-xl mt-10'>
+      <Skeleton />
+      <div className="flex gap-3">
+        <Skeleton width="4rem" />
+        <Skeleton width="5rem" />
+      </div>
+      <div className="mt-5 h-40 max-w-4xl">
+        <Skeleton count={5} />
+      </div>
+    </section>
+  )
+}
+
+export default SingleIssueLoading
