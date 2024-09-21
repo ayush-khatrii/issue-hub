5d36139ea5ada25c3b3279c09a8db2383251e446
Ayush Khatri <khatriayush2003@gmail.com>
2024-09-21 21:08:11 +0530

    Implemented data fetching with react query

--- app/issues/[id]/AssignIssue.tsx
+++ app/issues/[id]/AssignIssue.tsx
@@ -1,46 +1,38 @@
 "use client"
 import { Select, SelectItem } from "@nextui-org/react"
 import { User } from "@prisma/client";
-import { useEffect, useState } from "react";
-
+import { useQuery } from "@tanstack/react-query"
+import SelectSkeleton from "@/components/SelectSkeleton";
 export default function AssignIssue() {
-  const [users, setUsers] = useState<User[]>([]);
 
-  useEffect(() => {
-    async function fetchAllUsers() {
-      try {
-        const response = await fetch("/api/users");
-        const data = await response.json();
-        setUsers(data);
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchAllUsers();
-  }, []);
 
-  function hanadleChange() {
-    try {
-      // assign issue to user
-      alert("Issue assigned successfully");
-    } catch (error) {
-      // handle error
-      console.error(error);
-    }
-  }
+  const { isLoading, error, data: users } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const response = await fetch("/api/users");
+      const data = await response.json();
+      return data;
+    },
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
+
+
+  if (error) return null;
+  if (isLoading) return <SelectSkeleton />
 
   return (
     <>
       <Select
-        onChange={hanadleChange}
+        // onChange={hanadleChange}
         variant="bordered"
         label="Assign to"
         radius="sm"
         className="w-full md:w-64">
-        {users.map((user) => (
-          <SelectItem key={user.id}>{user.name}</SelectItem>
-        ))}
+        {
+          users?.map((user) => (
+            <SelectItem key={user.id}>{user.name}</SelectItem>
+          ))}
       </Select>
     </>
   )
---
+++ components/SelectSkeleton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/react";
+import Skeleton from "react-loading-skeleton";
+
+export default function SelectSkeleton() {
+  return (
+    <div className="w-full">
+      <Skeleton height="30px" />
+    </div>
+  )
+}
